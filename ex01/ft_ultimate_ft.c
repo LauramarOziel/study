47f7a934235ff5e232492a1baad9f6e90f1310c7
CW-Laura <lauramar.araujo@cloudwalk.io>
2025-09-14 17:57:34 -0300

    add ex00 to ex06

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loziel-d <loziel-d@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/13 15:18:38 by loziel-d          #+#    #+#             */
+/*   Updated: 2025/09/13 18:34:03 by loziel-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	nbr = 10;
+	int	*ptr1 = &nbr;
+	int	**ptr2 = &ptr1;
+	int	***ptr3 = &ptr2;
+	int	****ptr4 = &ptr3;
+	int	*****ptr5 = &ptr4;
+	int	******ptr6 = &ptr5;
+	int	*******ptr7 = &ptr6;
+	int	********ptr8 = &ptr7;
+	int	*********ptr9 = &ptr8;
+
+	printf("before: %d\n", nbr);
+	ft_ultimate_ft(ptr9);
+	printf("after: %d\n", nbr);
+	return (0);
+}
+*/
