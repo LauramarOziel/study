d09a8f6d56bf889aed59ae4eb4419ede475145db
LauramarOziel <lauramaroziel@gmail.com>
2025-09-15 00:51:08 -0300

    add ex00

---
+++ c02/ex00/ft_strcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: loziel-d <loziel-d@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/14 22:10:46 by loziel-d          #+#    #+#             */
+/*   Updated: 2025/09/15 00:50:34 by loziel-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	char	*original_dest;
+
+	original_dest = dest;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (original_dest);
+}
