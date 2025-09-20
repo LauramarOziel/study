08cb2fd6e4476585435bcc193b90f3e9e0ca955a
Laura <140969594+LauramarOziel@users.noreply.github.com>
2025-09-20 15:59:36 -0300

    Update ft_strncmp.c

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -14,8 +14,6 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
-	if (n == 0)
-		return (0);
 	i = 0;
 	while (i < n && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
